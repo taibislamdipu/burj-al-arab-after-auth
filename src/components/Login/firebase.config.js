15e230d2b49a02fe97e6f330bb02b474d3579e37
taibislamdipu <mailtaibislam@gmail.com>
2020-09-29 20:31:36 +0600

    only user id send to database

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBH7-80n3l3tRbFfYN4DlrodXEw3lcCtww",
+  authDomain: "burj-al-arab-after-auth.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-after-auth.firebaseio.com",
+  projectId: "burj-al-arab-after-auth",
+  storageBucket: "burj-al-arab-after-auth.appspot.com",
+  messagingSenderId: "821324549182",
+  appId: "1:821324549182:web:ec88b38d982b4e8bdf156f"
+};
+
+export default firebaseConfig;
