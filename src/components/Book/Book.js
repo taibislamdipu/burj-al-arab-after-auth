313e283a286cd725d33af6bc45e5db7515c95372
taibislamdipu <mailtaibislam@gmail.com>
2020-09-29 22:35:13 +0600

    insert data successfully into mongodb

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -38,7 +38,7 @@ const Book = () => {
         const newBooking = { ...loggedInUser, ...selectedDate };
         fetch('http://localhost:5000/addBooking', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/' },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(newBooking)
         })
             .then(res => res.json())
